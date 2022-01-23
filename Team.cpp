8424ea7807f492806d3c37267335616aa354c49b
TITANsj <suryanshj8@gmail.com>
2022-01-23 23:21:22 +0530

    Team

---
+++ Team.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int p,v,t,n,count=0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>p>>v>>t;
+        if(p+v+t>=2)
+            count++;
+    }
+    cout<<count<<endl;
+    return 0;
+}
